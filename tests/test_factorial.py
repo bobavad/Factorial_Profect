0f1b6f33357ddb26235c149fe73cd16ce38e1bb8
boba <97602524+bobavad@users.noreply.github.com>
2024-05-25 15:03:39 +0300

    Update test_factorial.py

--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -1,28 +1,28 @@
 from Project.factorial import factorial
 
 def test_factorial_zero():
-     """! 1.
+    """! 1.
     @param 0   Num 1.
     @return  1.
     """
     assert factorial(0) == 1
 
 def test_factorial_one():
-     """! 2.
+    """! 2.
     @param 1  Num 1.
     @return  1.
     """
     assert factorial(1) == 1
 
 def test_factorial_positive():
-     """! 3.
+    """! 3.
     @param 5  Num 1.
     @return  120.
     """
     assert factorial(5) == 120
     
 def test_4():
-     """! 4.
+    """! 4.
     @param 3   Num 1.
     @return  6.
     """
